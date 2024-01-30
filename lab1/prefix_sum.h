b6e750d70dd92912de9aa689a32f0b257d22cfbd
Arianna Azizi <92657037+ar1a3131@users.noreply.github.com>
2024-01-29 23:56:31 -0500

    Add files via upload

---
+++ lab1/prefix_sum.h
@@ -0,0 +1,8 @@
+#ifndef _PREFIX_SUM_H
+#define _PREFIX_SUM_H
+
+int prefix_sum(int ary[],int length);
+bool non_neg_prefix_sum(int ary[],int length);
+bool non_pos_prefix_sum(int ary[],int length);
+
+#endif 
